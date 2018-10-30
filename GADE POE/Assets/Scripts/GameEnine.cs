5d3b829116706f1f1ffd958e23c59f4a3b97a06c
KiwiCowan <kylecowan3@gmail.com>
2018-10-30 14:54:16 +0200

    unity

---
+++ GADE POE/Assets/Scripts/GameEnine.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+
+
+public class GameEngine
+{
+    public Map map = new Map();
+    public ResourceBuilding resources = new ResourceBuilding(0, 0, ".", ".");
+    bool playing = true;
+
+    public GameEngine()
+    {
+
+    }
+
+    public bool Playing
+    {
+        get
+        {
+            return playing;
+        }
+    }
+
+    public void PlayGame()
+    {
+        map.UnitSpawner();
+        map.BuildingSpawner();
+
+        for (int j = 0; j < map.buildings.Length; j++)
+        {
+            if (map.buildings[j] != null)
+                map.GameMap[map.buildings[j].YPos, map.buildings[j].XPos] = map.buildings[j].Symbol;
+        }
+    }
+
+    public void StartGame()
+    {
+
+        for (int j = 0; j < map.units.Length; j++) //runs though all units 
+        {
+            if (map.units[j] != null)
+            {
+                map.GameMap[map.units[j].YPos, map.units[j].XPos] = ",";
+
+                if (map.units[j].Hp > 0)
+                {
+
+                    //int ClosestUnit = map.units[j].ClosestUnitPos(map.units, j);
+                    //System.Console.WriteLine(j + " unit " + map.units[j].Faction + " finds  unit " + ClosestUnit + " , " + map.units[j].Faction + " at" + map.units[ClosestUnit].XPos + " , "  + map.units[ClosestUnit].YPos);
+                    if (map.units[j].Hp / map.units[j].MaxHP * 100 > 25 / 100)
+                    {
+
+                        if (map.units[j].AttackRangeCheck(map.units[j].ClosestUnit(map.units)) == true)
+                        {
+                            map.units[j].Combat(map.units[j].ClosestUnit(map.units));
+                        }
+                        else
+                        {
+                            map.units[j].MoveUnitPos(map.units[j].ClosestUnit(map.units));
+                            map.GameMap[map.units[j].YPos, map.units[j].XPos] = map.units[j].Symbol;
+                        }
+                    }
+                    else
+                    {
+                        map.units[j].RunRandom();
+                    }
+                }
+                else
+                {
+                    map.GameMap[map.units[j].YPos, map.units[j].XPos] = ",";
+                    map.units[j] = null;
+                }
+            }
+            //System.Console.WriteLine(map.units[j].ToString());
+            //Unit clostest = map.units[j].ClosestUnit(map.units);
+            //System.Console.WriteLine("unit " + j + "is looking for " + clostest.ToString());
+        }
+    }
+}
+
---
+++ GADE POE/Assets/Scripts/Units.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+
+
+    public abstract class Unit
+    {
+        //varibles
+        protected int xPos;
+        protected int yPos;
+        protected int hp;
+        protected int atk;
+        protected int range;
+        protected string faction;
+        protected string symbol;
+        protected int maxHP;
+        protected bool attacking;
+        //getters and setters
+        public int XPos
+        {
+            get
+            {
+                return xPos;
+            }
+            set
+            {
+                xPos = value;
+            }
+        }
+
+        public int YPos
+        {
+            get
+            {
+                return yPos;
+            }
+            set
+            {
+                yPos = value;
+            }
+        }
+        public int Hp
+        {
+            get
+            {
+                return hp;
+            }
+            set
+            {
+                hp = value;
+            }
+        }
+        public int Atk
+        {
+            get
+            {
+                return atk;
+            } set
+            {
+                atk = value;
+            }
+        }
+
+        public int Range
+        {
+            get
+            {
+                return range;
+            }
+
+            set
+            {
+                range = value;
+            }
+        }
+
+        public string Faction
+        {
+            get
+            {
+                return faction;
+            }
+
+            set
+            {
+                faction = value;
+            }
+        }
+
+        public string Symbol
+        {
+            get
+            {
+                return symbol;
+            }
+
+            set
+            {
+                symbol = value;
+            }
+        }
+
+        public int MaxHP
+        {
+            get
+            {
+                return maxHP;
+            }
+
+            set
+            {
+                maxHP = value;
+            }
+        }
+
+        public bool Attacking
+        {
+            get
+            {
+                return attacking;
+            }
+
+            set
+            {
+                attacking = value;
+            }
+        }
+
+        public Unit(int Xpos, int Ypos, string faction, string symbol)
+        {
+        }
+
+        public abstract Unit ClosestUnit(Unit[] units);
+
+        public abstract bool AttackRangeCheck(Unit unit);
+
+        public abstract void MoveUnitPos(Unit unit);
+
+        public abstract void Combat(Unit unit);
+
+        public abstract void RunRandom();
+
+
+
+    }
+
+
